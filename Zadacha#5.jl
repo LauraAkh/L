97980edea0db19b4a05922b10f7472a088a7fae9
LauraAkh <121516109+LauraAkh@users.noreply.github.com>
2022-12-28 00:30:50 +0300

    Add files via upload

---
+++ Zadacha#5.jl
@@ -0,0 +1,101 @@
+#5
+import HorizonSideRobots.move!
+function mark_rec!(r::Robot)
+    steps = get_left_down_angle_b!(r)
+
+    while isborder(r, Sud) && !isborder(r, Ost)
+        move_until_border!(r, Nord)
+        move!(r, Ost)
+        while !isborder(r, Ost) && move_if_possible!(r, Sud) end
+    end
+
+    for sides in [(Sud, Ost), (Ost, Nord), (Nord, West), (West, Sud)]
+        side_to_move, side_to_border = sides
+        while isborder(r, side_to_border)
+            putmarker!(r)
+            move!(r, side_to_move)
+        end
+        putmarker!(r)
+        move!(r, side_to_border)
+    end
+
+    get_left_down_angle_b!(r)
+    mark_perimetr!(r)
+    make_way_back!(r, steps)
+end
+
+function get_left_down_angle_b!(r::Robot)::Vector{Tuple{HorizonSide, Int}}
+    steps = []
+    while !(isborder(r, West) && isborder(r, Sud))
+        steps_to_West = move_until_border!(r, West)
+        steps_to_Sud = move_until_border!(r, Sud)
+        push!(steps, (West, steps_to_West))
+        push!(steps, (Sud, steps_to_Sud))
+    end
+    return steps
+end
+
+function move_if_possible!(r::Robot, side::HorizonSide)::Bool
+    if !isborder(r, side)
+        move!(r, side)
+        return true
+    end
+    return false
+end
+function inversed_path(path::Vector{Tuple{HorizonSide, Int}})::Vector{Tuple{HorizonSide, Int}}
+    inv_path = []
+    for step in path
+        inv_step = (inverse_side(step[1]), step[2])
+        push!(inv_path, inv_step)
+    end
+    reverse!(inv_path)
+    return inv_path
+end
+
+function make_way!(r::Robot, path::Vector{Tuple{HorizonSide, Int}})
+    for step in path
+        moves!(r, step[1], step[2])
+    end
+end
+
+function make_way_back!(r::Robot, path::Vector{Tuple{HorizonSide, Int}})
+    inv_path = inversed_path(path)
+    make_way!(r, inv_path)
+end
+
+function moves!(r::Robot, side::HorizonSide, n_steps::Int)
+    for _ in 1:n_steps
+        move!(r, side)
+    end
+end
+function mark_perimetr!(r::Robot)
+    steps_to_left_down_angle = [0, 0]
+    steps_to_left_down_angle[1] = move_until_border!(r, Sud)
+    steps_to_left_down_angle[2] = move_until_border!(r, West)
+    for side in (Nord, Ost, Sud, West)
+        putmarkers_until_border!(r, side)
+    end
+    moves!(r, Ost, steps_to_left_down_angle[2])
+    moves!(r, Nord, steps_to_left_down_angle[1])
+end
+function putmarkers_until_border!(r::Robot, side::HorizonSide)::Int
+    n_steps = 0
+    while !isborder(r, side) 
+        move!(r, side)
+        putmarker!(r)
+        n_steps += 1
+    end 
+    return n_steps
+end
+function move_until_border!(r::Robot, side::HorizonSide)::Int
+    n_steps = 0
+    while !isborder(r, side)
+        n_steps += 1
+        move!(r, side)
+    end
+    return n_steps
+end
+function inverse_side(side::HorizonSide)::HorizonSide
+    inv_side = HorizonSide((Int(side) + 2) % 4)
+    return inv_side
+end
